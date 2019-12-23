d74dae8f4c1c1001bb6dfbe608e055e75b10365e
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-12-23 12:56:22 -0600

    Added build folder of frontend application to backend

---
+++ build/precache-manifest.90fc8be51b2069f476bab30da8d42f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351641755d2fb9c485b1bdbc2daa733a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68db0b8b25aa9e01e983",
+    "url": "/static/js/2.0b713afe.chunk.js"
+  },
+  {
+    "revision": "1786c40e0edef31e1df4f59a2b6c93fa",
+    "url": "/static/js/2.0b713afe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf446c9d68b864dce5c9",
+    "url": "/static/js/main.7632bde3.chunk.js"
+  },
+  {
+    "revision": "31bfc067a06b18f0c566",
+    "url": "/static/js/runtime-main.bbc88e4f.js"
+  }
+]);
