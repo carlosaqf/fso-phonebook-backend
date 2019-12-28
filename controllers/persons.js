4bdbcc537594adb5361cc67dd805dfebab2534c3
carlosaqf <carlosfegurgur@gmail.com>
2019-12-28 14:20:32 -0600

    refactor routes to use async/await

--- controllers/persons.js
+++ controllers/persons.js
@@ -4,64 +4,94 @@ const personRouter = require('express').Router()
 const Person = require('../models/person')
 
 // GET ALL PERSONS
-personRouter.get('/', (req,res) => {
-    Person.find({}).then(persons => {
-        res.json(persons.map(person => person.toJSON()))
-    })
+personRouter.get('/', async (req,res) => {
+	const persons = await Person.find({})
+	res.json(persons.map(person => person.toJSON()))
+
+	// Person.find({}).then(persons => {
+	// 	res.json(persons.map(person => person.toJSON()))
+	// })
 })
 
 // GET SINGLE PERSON
-personRouter.get('/:id', (req,res,next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person.toJSON())
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+personRouter.get('/:id', async (req,res,next) => {
+
+	try{
+		const person = await Person.findById(req.params.id)
+		if (person){
+			res.json(person.toJSON())
+		} else {
+			res.status(404).end()
+		}
+	}catch(exception){
+		next(exception)
+	}
+
+	// Person.findById(req.params.id)
+	// 	.then(person => {
+	// 		if (person) {
+	// 			res.json(person.toJSON())
+	// 		} else {
+	// 			res.status(404).end()
+	// 		}
+	// 	})
+	// 	.catch(error => next(error))
 })
 
 // ADD SINGLE PERSON
-personRouter.post('/', (req,res, next) => {
-    const body = req.body
+personRouter.post('/', async (req,res, next) => {
+	const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
 
-    person.save()
-        .then(savedPerson => {
-            res.json(savedPerson.toJSON())
-        })
-        .catch(error => next(error))
+	try{
+		const savedPerson = await person.save()
+		res.json(savedPerson.toJSON())
+	}catch(exception){
+		next(exception)
+	}
+
+	// person.save()
+	// 	.then(savedPerson => {
+	// 		res.json(savedPerson.toJSON())
+	// 	})
+	// 	.catch(error => next(error))
 })
 
 // DELETE SINGLE PERSON
-personRouter.delete('/:id', (req,res,next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(() => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+personRouter.delete('/:id', async (req,res,next) => {
+
+	try {
+		await Person.findByIdAndRemove(req.params.id)
+		res.status(204).end()
+	}catch(exception){
+		next(exception)
+	}
+
+	// Person.findByIdAndRemove(req.params.id)
+	// 	.then(() => {
+	// 		res.status(204).end()
+	// 	})
+	// 	.catch(error => next(error))
 })
 
 // UPDATE SINGLE PERSON
 personRouter.put('/:id', (req,res,next) => {
-    const body = req.body
+	const body = req.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+	const person = {
+		name: body.name,
+		number: body.number
+	}
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson.toJSON())
-        })
-        .catch(error => next(error))
+	Person.findByIdAndUpdate(req.params.id, person, { new: true })
+		.then(updatedPerson => {
+			res.json(updatedPerson.toJSON())
+		})
+		.catch(error => next(error))
 })
 
 module.exports = personRouter
