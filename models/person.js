ea479ad9b330b1e52026a4ddbdd9a839c91832d6
carlosaqf <carlosfegurgur@gmail.com>
2019-12-28 14:20:13 -0600

    fixed syntax

--- models/person.js
+++ models/person.js
@@ -35,19 +35,19 @@
 const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    number: String
+	name: {
+		type: String,
+		required: true
+	},
+	number: String
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__V
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__V
+	}
 })
 
 module.exports = mongoose.model('Person', personSchema)
